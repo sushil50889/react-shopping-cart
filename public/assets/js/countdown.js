f5f0548ef96feba9b51a050679ef25c6e427af4d
sushil50889 <mandi.sushil@gmail.com>
2019-05-12 22:23:41 +0530

    countdoen error fix

--- public/assets/js/countdown.js
+++ public/assets/js/countdown.js
@@ -15,18 +15,18 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+  // var daysSpan = clock.querySelector('.days');
+  // var hoursSpan = clock.querySelector('.hours');
+  // var minutesSpan = clock.querySelector('.minutes');
+  // var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    // daysSpan.innerHTML = t.days;
+    // hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    // minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    // secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
